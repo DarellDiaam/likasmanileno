5f910b2418fa0882687d5c0e697725de5b592664
DarellDiaam <diamondcalalang@gmail.com>
2022-08-28 21:01:08 +0800

    at Widget.jsx, Dashboard cards has its own function

--- likasmanileno/src/components/widget/Widget.jsx
+++ likasmanileno/src/components/widget/Widget.jsx
@@ -7,18 +7,30 @@ import ElderlyIcon from '@mui/icons-material/Elderly';
 import axios from "axios";
 import React,{useState,useEffect} from 'react'
 const Widget = ({ type }) => {
-    let data;
+    let user;
+    let senior;
+    let families;
+    let sites;
+
     //temporary
     // const amount = 50000;
     //function para makuha yung bilang ng nasa table
     const diff = 30;
-    const[totalUsers,setTotalUser] = useState([])
+    const[totalUsers,setTotalUser,] = useState([])
+    const[totalSeniors,setTotalSenior,] = useState([])
+    const[totalFamilies,setTotalFamilies,] = useState([])
+    const[totalSites,setTotalSites,] = useState([])
+
+
     useEffect(() => {
         const fetchPosts = async () => {
             axios.post('http://localhost:4000/app/getUsers')
                 .then(res => {
                     console.log(res);
-                    setTotalUser(res.data.length);
+                    setTotalUser(res.user.length);
+                    setTotalSenior(res.senior.length);
+                    setTotalFamilies(res.sites.length);
+                    setTotalSites(res.sites.length);
                 }).catch(err => {
                     console.log(err);
                 })
@@ -27,7 +39,7 @@ const Widget = ({ type }) => {
     }, []);
     switch(type){
         case"newuser":
-        data={
+        user={
             title:"NEW USERS",
             isUser: false,
             link:"Compared to last month",
@@ -36,7 +48,7 @@ const Widget = ({ type }) => {
         };
         break;
         case"senior":
-        data={
+        senior={
             title:"SENIOR CITIZENS",
             isUser: true,
             link:"Compared to last month",
@@ -44,7 +56,7 @@ const Widget = ({ type }) => {
         };
         break;
         case"families":
-        data={
+        families={
             title:"NUMBER OF FAMILIES",
             isUser: false,
             link:"Compared to last month",
@@ -52,7 +64,7 @@ const Widget = ({ type }) => {
         };
         break;
         case"evacuation":
-        data={
+        sites={
             title:"EVACUATION SITES",
             isUser: true,
             link:"Compared to last month",
@@ -68,16 +80,37 @@ const Widget = ({ type }) => {
   return (
     <div className="widget">
         <div className="left">
-        <span className="title">{data.title}</span>
-        <span className="counter">{data.isUser} {totalUsers} </span>
-        <span className="link">{data.link}</span>
+        <span className="title">{user.title}</span>
+        <span className="counter">{user.isUser} {totalUsers} </span>
+        <span className="link">{user.link}</span>
+        </div>
+        
+        <div className="left">
+        <span className="title">{senior.title}</span>
+        <span className="counter">{senior.isUser} {totalSeniors} </span>
+        <span className="link">{senior.link}</span>
+        </div>
+
+        <div className="left">
+        <span className="title">{families.title}</span> 
+        <span className="counter">{families.isUser} {totalFamilies} </span>
+        <span className="link">{families.link}</span>
+        </div>
+
+        <div className="left">
+        <span className="title">{sites.title}</span> 
+        <span className="counter">{sites.isUser} {totalSites} </span>
+        <span className="link">{sites.link}</span>
         </div>
         <div className="right">
             <div className="percentage positive">
                 <ArrowDropUpIcon/>
                 {diff}  %
             </div>
-            {data.icon}
+            {user.icon}
+            {senior.icon}
+            {families.icon}
+            {sites.icon}
         </div>
     </div>
   )
